18efb637a8897e3a4a42d1f787d55df247994930
Xmanliujun <644240319@qq.com>
2013-07-17 11:09:00 +0800

    Initial Commit

---
+++ MySqliteArc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySqliteArc
+//
+//  Created by 刘军 on 13-7-17.
+//  Copyright (c) 2013年 刘军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
